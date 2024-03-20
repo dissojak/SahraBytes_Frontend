ea3b9169c104e73b3e9dfa5ab930a46cb86515e0
Adem Ben Amor <dissojak@gmail.com>
2024-03-20 06:37:26 +0100

    Updates

---
+++ precache-manifest.917cddb58a325f438721e0f5470e1641.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faefe313dbd23b8cd4152509d215851c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf3d4568d85bacd47f3",
+    "url": "/static/css/main.02fa5ad2.chunk.css"
+  },
+  {
+    "revision": "506191b66ff30f8736e1",
+    "url": "/static/js/2.8764c684.chunk.js"
+  },
+  {
+    "revision": "eaf3d4568d85bacd47f3",
+    "url": "/static/js/main.fde2ed3e.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  },
+  {
+    "revision": "a0ffdc02427eba7e57df5d5c32836515",
+    "url": "/static/media/Arimo-Bold.a0ffdc02.ttf"
+  },
+  {
+    "revision": "d9cc167ee50270d831c5f35ea5018f14",
+    "url": "/static/media/Arimo-Regular.d9cc167e.ttf"
+  },
+  {
+    "revision": "b2b293064f557c41411aac04d6f6710d",
+    "url": "/static/media/BebasNeue-Regular.b2b29306.ttf"
+  },
+  {
+    "revision": "1d8b73517fb37be5cf23cd3843ad4eb2",
+    "url": "/static/media/Raleway-ExtraBold.1d8b7351.ttf"
+  },
+  {
+    "revision": "d95649da7dfb965a289ac29105ce8771",
+    "url": "/static/media/Raleway-Regular.d95649da.ttf"
+  },
+  {
+    "revision": "804eb7bc11d3a38d5f5e108f8d1d1acd",
+    "url": "/static/media/Raleway-SemiBold.804eb7bc.ttf"
+  },
+  {
+    "revision": "b8e42971dec8d49207a8c8e2b919a6ac",
+    "url": "/static/media/Roboto-Bold.b8e42971.ttf"
+  },
+  {
+    "revision": "68ea4734cf86bd544650aee05137d7bb",
+    "url": "/static/media/Roboto-Medium.68ea4734.ttf"
+  },
+  {
+    "revision": "896a60219f6157eab096825a0c9348a8",
+    "url": "/static/media/Ubuntu-Bold.896a6021.ttf"
+  }
+]);
